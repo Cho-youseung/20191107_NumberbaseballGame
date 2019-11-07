a5ff97cc91dd3041d5f7f94c583ed4606b093f07
Cho-youseung <seckblue@gmail.com>
2019-11-07 19:45:25 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeunit/a20191107_numberbaseballgame/BaseActvity.kt
@@ -0,0 +1,10 @@
+package com.tjoeunit.a20191107_numberbaseballgame
+
+import android.appcompt.app.AppCompatActvity
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity
+
+
+class BaseActvity {
+}
